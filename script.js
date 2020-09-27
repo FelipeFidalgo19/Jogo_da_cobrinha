76b8a25742b3d722012403dde13cacc0941add8e
FelipeFidalgo19 <49208754+FelipeFidalgo19@users.noreply.github.com>
2020-09-27 09:28:28 -0300

    original

---
+++ script.js
@@ -0,0 +1,13 @@
+var login = document.getElementById('login');
+var senha = document.getElementById('senha');
+var login_correto = 'felipe';
+var login_senha = '0000';
+
+function jogando(){
+		if (login == login_correto){
+		window.alert('esta funcionando');
+	}else{
+		window.alert('login incorreto, tente novamente');
+	}
+ window.alert('Faça o login')
+}
